622b0a0ce9f904f21f8903e2022b1d7bba6b03a8
Sergey Kozachuk <skozachuk91@yandex.ru>
2021-02-04 23:53:54 +0300

    Began to write a script for mobile menu

---
+++ js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+    
+    const menuToggle = document.querySelector('.toggle-menu')
+    menuToggle.addEventListener('click', function(){
+        
+    });
+    // Hear to event
+
+
+})
