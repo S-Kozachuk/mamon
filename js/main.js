b2e49d5ebe18794d9ab3695966b4e1f3150ade43
Sergey Kozachuk <skozachuk91@yandex.ru>
2021-02-06 08:47:28 +0300

    Added script for Hamburger menu and stylize

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
-$(document).ready(function() {
-    
-    const menuToggle = document.querySelector('.toggle-menu')
-    menuToggle.addEventListener('click', function(){
-        
-    });
-    // Hear to event
+$(document).ready(function () {
 
+    const menuToggle = document.querySelector('.toggle-menu');
+
+    menuToggle.addEventListener('click', function(){
+        this.classList.toggle('active');
+
+    });
 
 })
