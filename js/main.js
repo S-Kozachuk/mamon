58fdb671fd5326bd4b9a7541760680b5ea21457a
Sergey Kozachuk <skozachuk91@yandex.ru>
2021-02-07 08:41:42 +0300

    Added close side menu effect when to click on link

--- js/main.js
+++ js/main.js
@@ -5,11 +5,20 @@ $(document).ready(function () {
     const overlayEl = document.querySelector('#overlay');
     const bodyEl = document.body;
 
+    //Click to Hamburger icon
     menuToggle.addEventListener('click', function(){
         this.classList.toggle('active');
         mobMenu.classList.toggle('active');
         overlayEl.classList.toggle('active');
         bodyEl.classList.toggle('noscroll');
     });
+
+    //Click to Mobile memu
+    mobMenu.addEventListener('click', function(){
+        this.classList.remove('active');
+        menuToggle.classList.remove('active');
+        overlayEl.classList.remove('active');
+        bodyEl.classList.remove('noscroll');
+    })
 
 })
