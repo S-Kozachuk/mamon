dd118ea9cb6c64ddb2c0f251c19373fbbd5572a1
Sergey Kozachuk <skozachuk91@yandex.ru>
2021-02-07 07:36:37 +0300

    Enabled side effect for mobile menu

--- js/main.js
+++ js/main.js
@@ -1,10 +1,11 @@
 $(document).ready(function () {
 
     const menuToggle = document.querySelector('.toggle-menu');
+    const mobMenu = document.querySelector('.nav');
 
     menuToggle.addEventListener('click', function(){
         this.classList.toggle('active');
-
+        mobMenu.classList.toggle('active');
     });
 
 })
