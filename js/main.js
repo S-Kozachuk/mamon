ebb9f8bdd36f7d80f65dfbb23ae8ab9ff76276ee
Sergey Kozachuk <skozachuk91@yandex.ru>
2021-02-07 08:20:54 +0300

    Fixed body to unscroll effect

--- js/main.js
+++ js/main.js
@@ -3,11 +3,13 @@ $(document).ready(function () {
     const menuToggle = document.querySelector('.toggle-menu');
     const mobMenu = document.querySelector('.nav');
     const overlayEl = document.querySelector('#overlay');
+    const bodyEl = document.body;
 
     menuToggle.addEventListener('click', function(){
         this.classList.toggle('active');
         mobMenu.classList.toggle('active');
         overlayEl.classList.toggle('active');
+        bodyEl.classList.toggle('noscroll');
     });
 
 })
