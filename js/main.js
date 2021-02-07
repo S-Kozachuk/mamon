cdcca1e4064fc98ee2aca66da90344cac2645697
Sergey Kozachuk <skozachuk91@yandex.ru>
2021-02-07 08:05:16 +0300

    Finished enable the side effect script

--- js/main.js
+++ js/main.js
@@ -2,10 +2,12 @@ $(document).ready(function () {
 
     const menuToggle = document.querySelector('.toggle-menu');
     const mobMenu = document.querySelector('.nav');
+    const overlayEl = document.querySelector('#overlay');
 
     menuToggle.addEventListener('click', function(){
         this.classList.toggle('active');
         mobMenu.classList.toggle('active');
+        overlayEl.classList.toggle('active');
     });
 
 })
