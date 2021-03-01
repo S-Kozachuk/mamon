8ecda8a168912f1a4b6df44af59744a63edb096c
Sergey Kozachuk <skozachuk91@yandex.ru>
2021-03-01 09:31:50 +0300

    SCRIPT / Added scroll JS script

--- js/main.js
+++ js/main.js
@@ -84,4 +84,10 @@ $(document).ready(function () {
 
     });
 
+    $("body").on('click', '[href*="#"]', function(e){
+        var fixed_offset = 100;
+        $('html,body').stop().animate({ scrollTop: $(this.hash).offset().top - fixed_offset }, 400);
+        e.preventDefault();
+      });
+
 })
