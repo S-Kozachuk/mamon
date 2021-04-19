f82db9401424bdf9a9c88cfda5ba6eaf8cd0d2b1
Sergey Kozachuk <skozachuk91@yandex.ru>
2021-04-19 18:01:45 +0300

    Added Form placeholder script

--- js/popup.js
+++ js/popup.js
@@ -132,3 +132,19 @@ document.addEventListener ('keydown', function (e) {
             Element.prototype.msMatchesSelector;
     }
 })();
+
+// Form placeholder
+const formInputs = document.querySelectorAll('.form-field');
+for (let item of formInputs){
+    const thisPlaceholder = item.nextElementSibling;
+    
+    item.addEventListener('focus', function(){
+        thisPlaceholder.classList.add('active');
+    });
+
+    item.addEventListener('blur', function(){
+        if(item.value == '') {
+            thisPlaceholder.classList.remove('active');
+        }
+    }); 
+}
