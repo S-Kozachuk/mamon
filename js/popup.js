7ca5c21b39c413a00adeb6ae57c46d6cc104a5aa
Sergey Kozachuk <skozachuk91@yandex.ru>
2021-04-19 20:46:49 +0300

    Added post ajax script

--- js/popup.js
+++ js/popup.js
@@ -180,3 +180,26 @@ $('#contacts-form').validate({
     }
 });
 
+// Функция AJAX запроса на сервер
+    
+function ajaxFormSubmit() {
+
+    let string = $("#contacts-form").serialize(); // Соханяем данные введенные в форму в строку.
+
+    //Формируем ajax запрос
+    $.ajax({
+        type: "POST", // Тип запроса - POST
+        url: "php/mail.php", // Куда отправляем запрос
+        data: string, // Какие даные отправляем, в данном случае отправляем переменную string
+
+        // Функция если все прошло успешно
+        success: function (html) {
+            $("#contacts-form").slideUp(800);
+            $('#answer').html(html);
+        }
+    });
+
+    // Чтобы по Submit больше ничего не выполнялось - делаем возврат false чтобы прервать цепчку срабатывания остальных функций
+    return false;
+}
+
